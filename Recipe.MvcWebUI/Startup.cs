54b939bab11f62d6fc0285913a079cfffd7fbd6d
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-07-06 20:50:18 +0300

    Configure Net Core Mvc App

--- Recipe.MvcWebUI/Startup.cs
+++ Recipe.MvcWebUI/Startup.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace Recipe.MvcWebUI
 {
@@ -15,6 +18,13 @@ namespace Recipe.MvcWebUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            IFileProvider physicalProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
+
+            services.AddSingleton<IFileProvider>(physicalProvider);
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+            services.AddSession();
+            services.AddMvc();
+            services.AddDistributedMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +35,14 @@ namespace Recipe.MvcWebUI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseSession();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Anasayfa}/{action=Index}/{id?}");
             });
         }
     }
