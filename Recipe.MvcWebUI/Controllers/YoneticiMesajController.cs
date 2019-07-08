5f10f69d402e963542e67dc6d8a2994dbf4868aa
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-07-08 19:47:05 +0300

    Admin Contact Operations

---
+++ Recipe.MvcWebUI/Controllers/YoneticiMesajController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Recipe.Business.Concrete.LiteDb;
+using Recipe.Entities.Concrete;
+
+namespace Recipe.MvcWebUI.Controllers
+{
+    public class YoneticiMesajController : Controller
+    {
+
+        LDContactManager contactManager = new LDContactManager("Contacts");
+
+        public IActionResult Index()
+        {
+            var items = contactManager.GetAll();
+            return View(items);
+        }
+
+        public IActionResult Incele(int Id)
+        {
+            var item = contactManager.GetById(Id);
+            if (item == null)
+            {
+                return RedirectToAction("Hata", "Yonetici");
+            }
+            return View(item);
+        }
+
+        [HttpPost]
+        public IActionResult Sil(int Id)
+        {
+            contactManager.Delete(Id);
+            TempData["MesajSilmeBasariMesaj"] = "Mesaj Başarıyla Silindi...";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Recipe.MvcWebUI/Controllers/YoneticiUyeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Recipe.Business.Concrete.LiteDb;
+
+namespace Recipe.MvcWebUI.Controllers
+{
+    public class YoneticiUyeController : Controller
+    {
+
+        LDUserManager userManager = new LDUserManager("Users");
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
