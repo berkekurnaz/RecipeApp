1bf37fa2a465bf8b97fe76b6aa99e3a0fa9a5dd8
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-07-09 20:34:40 +0300

    Admin Comment Operations

---
+++ Recipe.MvcWebUI/Controllers/YoneticiYorumController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Recipe.Business.Concrete.LiteDb;
+using Recipe.Entities.Concrete;
+
+namespace Recipe.MvcWebUI.Controllers
+{
+    public class YoneticiYorumController : Controller
+    {
+
+        LDCommentManager commentManager = new LDCommentManager("Comments");
+
+        public IActionResult Index()
+        { 
+            var items = commentManager.GetAll();
+            return View(items);
+        }
+
+        public IActionResult Incele(int Id)
+        {
+            var comment = commentManager.GetById(Id);
+            if (comment == null)
+            {
+                return RedirectToAction("Hata", "Yonetici");
+            }
+            return View(comment);
+        }
+
+        [HttpPost]
+        public IActionResult Sil(int Id)
+        {
+            commentManager.Delete(Id);
+            TempData["YorumSilmeBasariMesaj"] = "Yorum Başarıyla Silindi...";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
