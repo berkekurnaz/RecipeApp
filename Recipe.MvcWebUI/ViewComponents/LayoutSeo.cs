099f67bbb8e6aa0b139b9a81965733d0dc9af03e
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-08-04 12:59:30 +0300

    Update Layout Screens

---
+++ Recipe.MvcWebUI/ViewComponents/LayoutSeo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Recipe.Business.Concrete.LiteDb;
+using Recipe.MvcWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe.MvcWebUI.ViewComponents
+{
+    public class LayoutSeo : ViewComponent
+    {
+
+        LDCategoryManager categoryManager = new LDCategoryManager("Categories");
+        LDSettingManager settingManager = new LDSettingManager("Settings");
+
+        public IViewComponentResult Invoke()
+        {
+            MyLayoutViewModel myLayoutViewModel = new MyLayoutViewModel()
+            {
+                Setting = settingManager.GetById(1),
+                Categories = categoryManager.GetAll()
+            };
+            return View(myLayoutViewModel);
+        }
+
+    }
+}
